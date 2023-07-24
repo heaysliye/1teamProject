308f36e4ceeb006f5da22e90464c417d1f3517ea
heaysliye <140056272+heaysliye@users.noreply.github.com>
2023-07-24 12:09:15 +0900

    Add files via upload

---
+++ 1teamProject/src/main/ClassController.java
@@ -0,0 +1,36 @@
+package com.itwillbs.controller;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.itwillbs.domain.ClassDTO;
+import com.itwillbs.service.ClassService;
+
+
+@Controller
+public class ClassController {
+
+	
+	@Inject
+	private ClassService classService;
+	
+	@Inject
+	public void setMemberService(ClassService memberService) {
+		this.classService = memberService;
+	}
+	
+	@RequestMapping(value = "/main/property-single", method = RequestMethod.GET)
+	public String classList(ClassDTO classDTO) {
+	
+		return "main/property-single";
+	}//
+	
+	
+	
+	
+	
+}
+
